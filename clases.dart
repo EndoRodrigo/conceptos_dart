d6589291b31aa4804bb3253e14d607dfd6fbe311
EndoRodrigo <endorodrigo19@gmail.com>
2026-08-19 21:53:51 -0500

    feat: seimplementa transformacion de datos y sobreescribir metodos

--- clases.dart
+++ clases.dart
@@ -1,21 +1,33 @@
+Map<String,dynamic> eroe = {
+  'name': 'Endo',
+  'power':'Megamene',
+  'isAlive': true
+};
+
+
 void main(List<String> args) {
   final Hero wolwerin = Hero(name: 'wolwerin', power: 'Regeneracion');
   print(wolwerin.toString());
   print(wolwerin.name);
   print(wolwerin.power);
+  print(Hero.fromJson(eroe));
 }
 
 class Hero {
   String name;
   String power;
+  bool isAlive;
 
-  Hero({required this.name, required this.power});
+  Hero({required this.name, required this.power, this.isAlive=false});
+
+  Hero.fromJson(Map<String, dynamic> json)
+    :name = json['name'] ?? 'No named found',
+    power = json['power'] ?? 'No power found',
+    isAlive =json['isAlive'] ?? 'Mo power found';
 
   @override
   String toString() {
     // TODO: implement toString
-    return '$name - $power';
+    return '$name, $power, IsAlive: ${isAlive ? 'YES!' : 'nOT!'}';
   }
-
-
 }
